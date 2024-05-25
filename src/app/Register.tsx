8a282bc54e474d03d4d69a66b1dcdead223ec1ec
Saharat Suwannapapond <saharat.rus@gmail.com>
2024-05-25 22:50:37 +0700

    update

--- src/app/Register.tsx
+++ src/app/Register.tsx
@@ -1,46 +1,54 @@
 import React from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
+import { Container, Box, Typography, TextField, Button } from '@mui/material';
 import { Link } from "react-router-dom";
 
 const Register: React.FC = () => {
   return (
     <Container maxWidth="sm">
-      <Typography variant="h4" align="center" gutterBottom>
-        Register Page
-      </Typography>
-      <form>
-        <TextField
-          label="Username"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-        />
-        <TextField
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
-        >
-          Register
-        </Button>
-        <Link to="/login">ย้อนกลับ</Link>
-      </form>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+      >
+        <Typography variant="h4" align="center" gutterBottom>
+          Register Page
+        </Typography>
+        <form>
+          <TextField
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Register
+          </Button>
+          <Link to="/login">ย้อนกลับ</Link>
+        </form>
+      </Box>
     </Container>
   );
 };
